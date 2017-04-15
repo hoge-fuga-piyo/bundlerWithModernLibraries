7754fcbc8fa968ffeef31fca48b6e7bbd6d17328
hoge-fuga-piyo <nao07770@yahoo.co.jp>
2017-04-15 23:54:21 +0900

    必要な関数を追加

--- src/core/image_pair.cpp
+++ src/core/image_pair.cpp
@@ -1,1 +1,28 @@
 #include "image_pair.hpp"
+
+ImagePair::ImagePair(){
+
+}
+
+void ImagePair::keypointMatching(const std::vector<cv::KeyPoint>& kKeypoints1, const cv::Mat& descriptor1,
+                        const std::vector<cv::KeyPoint>& kKeypoint2, const cv::Mat& descriptor2){
+
+}
+
+/**
+ * @brief       set image indexes
+ * @param[in]   index1
+ * @param[in]   index2
+ */
+void ImagePair::setImageIndex(int index1, int index2) const{
+    index[0]=index1;
+    index[1]=index2;
+}
+
+/**
+ * @brief   get image indexes
+ * @return  image indexes
+ */
+std::array<int, 2> ImagePair::getImageIndex() const{
+    return index;
+}
--- src/core/image_pair.hpp
+++ src/core/image_pair.hpp
@@ -3,9 +3,20 @@
 #include <array>
 #include <opencv2/opencv.hpp>
 
+/**
+ * @brief This class is about processing and information between 2 images.
+ */
 class ImagePair{
-public:
-private:
+public:     //Methods
+    ImagePair();
+    void keypointMatching(const std::vector<cv::KeyPoint>& kKeypoints1, const cv::Mat& descriptor1,
+                        const std::vector<cv::KeyPoint>& kKeypoint2, const cv::Mat& descriptor2);
+
+public:     //Setter/Getter
+    void setImageIndex(int index1, int index2) const;
+    std::array<int, 2> getImageIndex() const;
+
+private:    //Instance variables
     std::array<int, 2> index;       //! index of two images
-    std::vector<cv::DMatch> match;
+    std::vector<cv::DMatch> match;  //! information of keypoint matching
 };
